94c0c005c90e8f47b4da0116e52790092c8323f7
aliyabear <aliya.hassam@gmail.com>
2017-07-07 15:11:22 -0700

    Keeping it simple

---
+++ Happiness/ColorSelectionCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ColorSelectionCollectionViewCell.swift
+//  Happiness
+//
+//  Created by Aliya Hassam on 2017-07-07.
+//  Copyright © 2017 Aliya Hassam. All rights reserved.
+//
+
+import UIKit
+
+class ColorSelectionCollectionViewCell: UICollectionViewCell {
+
+}
